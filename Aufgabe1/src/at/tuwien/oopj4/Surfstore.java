12b81bccd4c2902c5b002059231c6f45e107cea3
Tom Muhm <tom.muhm@gmail.com>
2014-10-10 16:45:49 +0200

    minor style changes

--- Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
+++ Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
@@ -7,22 +7,20 @@ public class Surfstore {
 	private ArrayList<Person> persons = new ArrayList<Person>();
 	private ArrayList<Surfarticle> articles = new ArrayList<Surfarticle>();
 
-	public void borrowSurfArticle() {
+  public void createPerson(String firstname, String lastname) {
+    persons.add(new Person(firstname + " " + lastname));
+  }
+
+  public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
+    articles.add(new Surfarticle(articelName, articleNumber, articlePrice));
+  }
+
+	public void borrowSurfarticle() {
 
 	}
 
 	public void returnSurfarticle() {
 
 	}
-
-
-	public void createPerson(String firstname, String lastname) {
-		persons.add(new Person(firstname + " " + lastname));
-	}
-
-	public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
-		articles.add(new Surfarticle(articelName, articleNumber, articlePrice));
-	}
-
 
 }
