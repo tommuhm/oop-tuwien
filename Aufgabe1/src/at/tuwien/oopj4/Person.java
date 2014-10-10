64e51b5764ea35a8ef905185ffe7bc1c7e36c4dc
o1327535 <e1327535@student.tuwien.ac.at>
2014-10-10 16:01:25 +0200

    Personenklasse hochladen.

--- Aufgabe1/src/at/tuwien/oopj4/Person.java
+++ Aufgabe1/src/at/tuwien/oopj4/Person.java
@@ -1,7 +1,34 @@
 package at.tuwien.oopj4;
 
 /**
- * Created by tom on 10/10/14.
+ * Created by tom, ulrich und david on 10/10/14.
  */
 public class Person {
+	private static int iMaxPersonID = 0;
+	
+	private String sPersonName;
+	private int iPersonID;
+	
+	public Person(String sPersonName)
+	{
+		this.sPersonName = sPersonName;
+		this.iPersonID = this.iMaxPersonID++; //Set PersonID and increment by 1
+	}
+
+	
+	//===========GETTER AND SETTERS===============
+	public String getsPersonName() {
+		return sPersonName;
+	}
+
+	public int getiPersonID() {
+		return iPersonID;
+	}
+	//============================================
+
+
+	@Override
+	public String toString() {
+		return this.iPersonID + " " + this.sPersonName;
+	}
 }
