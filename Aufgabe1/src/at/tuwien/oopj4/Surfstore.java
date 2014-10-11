e33817eedd8dab01ca0b7edd385883cbf50ee90b
Tom Muhm <tom.muhm@gmail.com>
2014-10-12 01:09:26 +0200

    fixed formatting

--- Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
+++ Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 public class Surfstore {
 
 	private ArrayList<Person> persons = new ArrayList<Person>();
-	private ArrayList<Surfarticle> articles = new ArrayList<Surfarticle>();
+	private ArrayList<Surfarticle> surfArticles = new ArrayList<Surfarticle>();
 
-  public void createPerson(String firstname, String lastname) {
-    persons.add(new Person(firstname + " " + lastname));
-  }
+	public void createPerson(String firstname, String lastname) {
+    		persons.add(new Person(firstname + " " + lastname));
+  	}
 
-  public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
-    articles.add(new Surfarticle(articelName, articleNumber, articlePrice));
-  }
+  	public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
+    		surfArticles.add(new Surfarticle(articelName, articleNumber, articlePrice));
+  	}
 
-	public void borrowSurfarticle() {
+	public void borrowSurfarticle(Person person, Surfarticle surfarticle, DateTime issueDate, int ) {
 
 	}
 
