e751d4f122236d92767729d4c0bb3e60b3cd5608
Tom Muhm <tom.muhm@gmail.com>
2014-10-12 11:03:22 +0200

    implemented borrowArticle

--- Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
+++ Aufgabe1/src/at/tuwien/oopj4/Surfstore.java
@@ -1,26 +1,62 @@
 package at.tuwien.oopj4;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Surfstore {
 
 	private ArrayList<Person> persons = new ArrayList<Person>();
 	private ArrayList<Surfarticle> surfArticles = new ArrayList<Surfarticle>();
+	private HashMap<Person, ArrayList<Rental>> rentedMap = new HashMap<Person, ArrayList<Rental>>();
 
 	public void createPerson(String firstname, String lastname) {
-    		persons.add(new Person(firstname + " " + lastname));
-  	}
+		persons.add(new Person(firstname + " " + lastname));
+	}
 
-  	public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
-    		surfArticles.add(new Surfarticle(articelName, articleNumber, articlePrice));
-  	}
+	public void createSurfarticle(String articelName, int articleNumber, float articlePrice) {
+		surfArticles.add(new Surfarticle(articelName, articleNumber, articlePrice));
+	}
 
-	public void borrowSurfarticle(Person person, Surfarticle surfarticle, DateTime issueDate, int ) {
+	public void borrowSurfarticle(Person person, Surfarticle surfarticle, int amount, Date issueDate) {
+		if (surfarticle.borrow(amount)) {
+			ArrayList<Rental> rentedArticles;
+			if (!rentedMap.containsKey(person)) {
+				rentedArticles= new ArrayList<Rental>();
+				rentedMap.put(person, rentedArticles);
+			} else {
+				rentedArticles = rentedMap.get(person);
+			}
+
+			for (int i = 0; i < amount; i++) {
+				rentedArticles.add(new Rental(surfarticle, issueDate));
+			}
+		} else {
+			System.out.println("Could not borrow " + surfarticle.getArticleName());
+		}
+	}
+
+	public void returnSurfarticle(Person person, Surfarticle surfarticle, int amount) {
+		if (rentedMap.containsKey(person)) {
+
+		} else {
+			System.out.println();
+		}
+
+		surfarticle.returnArticle();
 
 	}
 
-	public void returnSurfarticle() {
+	public void printSurfArticles() {
+		for (Surfarticle surfarticle : surfArticles) {
+			System.out.println(surfarticle);
+		}
+	}
 
+	public void printPersons() {
+		for (Person person : persons) {
+			System.out.println(person);
+		}
 	}
 
 }
