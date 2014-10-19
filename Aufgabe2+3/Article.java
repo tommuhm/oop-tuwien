8141db6de5d6ecc8483cc1f8356ab18f4dd22b71
Tom Muhm <tom.muhm@gmail.com>
2014-10-19 12:44:41 +0200

    refactoring: Article is now a abstract class

--- Aufgabe2+3/Article.java
+++ Aufgabe2+3/Article.java
@@ -2,30 +2,24 @@
  * @author: djaffry, uaschl, tmuhm
  */
 
-public class Article {
+public abstract class Article {
 
 	private static int maxArticleNumber = 0;
 
 	private final int id;
 	private String name;
 	private String size;
-	private float pricePerHour;
 	private int currentAmount;
 	private int totalAmount;
 	boolean isNew;
 
-	public Article(String name, String size, float pricePerHour, boolean isNew) {
+	public Article(String name, String size, boolean isNew) {
 		this.id = Article.maxArticleNumber++; //Set name and increment by 1
 		this.name = name;
 		this.size = size;
-		this.pricePerHour = pricePerHour;
 		this.totalAmount = 0;
 		this.currentAmount = totalAmount;
 		this.isNew = isNew;
-	}
-
-	public Article(Article a) {
-		this(a.getName(), a.getSize(), a.getPricePerHour(), a.isNew());
 	}
 
 	public int getId() {
@@ -38,10 +32,6 @@ public class Article {
 
 	public String getSize() {
 		return size;
-	}
-
-	public float getPricePerHour() {
-		return pricePerHour;
 	}
 
 	public int getCurrentAmount() {
@@ -81,15 +71,4 @@ public class Article {
 	public void returnArticle(int amount) {
 		currentAmount += amount;
 	}
-
-	public String toString() {
-		return "Name: " + this.name
-				+ ", ID: " + this.id
-				+ ", size: " + this.size + "cm"
-				+ ", price/Hour: " + this.pricePerHour
-				+ ", total: " + this.totalAmount
-				+ ", available: " + this.currentAmount
-				+ ", rented: " + (this.totalAmount - this.currentAmount);
-	}
-
 }
