58711dee1ed84acfd118cdb4db212a3ac7e63df5
Tom Muhm <tom.muhm@gmail.com>
2014-10-19 12:40:27 +0200

    ArticleRent, ArticleSale: added copy constructor ArticleRent: added pricePerDay

--- Aufgabe2+3/ArticleRent.java
+++ Aufgabe2+3/ArticleRent.java
@@ -1,14 +1,25 @@
 public class ArticleRent extends Article {
 
 	private float pricePerHour;
+	private float pricePerDay;
 
-	public ArticleRent(String name, String size, float pricePerHour, boolean isNew) {
+	public ArticleRent(String name, String size, float pricePerHour, float pricePerDay, boolean isNew) {
 		super(name, size, isNew);
 		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+	}
+
+	public ArticleRent(ArticleRent aricleRent) {
+		this(aricleRent.getName(), aricleRent.getSize(), aricleRent.getPricePerHour(),
+				aricleRent.getPricePerDay(), aricleRent.isNew());
 	}
 
 	public float getPricePerHour() {
 		return pricePerHour;
+	}
+
+	public float getPricePerDay() {
+		return pricePerDay;
 	}
 
 	public String toString() {
--- Aufgabe2+3/ArticleSale.java
+++ Aufgabe2+3/ArticleSale.java
@@ -4,6 +4,15 @@ public class ArticleSale extends Article {
 
 	public ArticleSale(String name, String size, float priceSale, boolean isNew) {
 		super(name, size, isNew);
+		this.priceSale = priceSale;
+	}
+
+	public ArticleSale(ArticleSale aricleSale) {
+		this(aricleSale.getName(), aricleSale.getSize(), aricleSale.priceSale, aricleSale.isNew());
+	}
+
+	public float getPriceSale() {
+		return priceSale;
 	}
 
 	public String toString() {
