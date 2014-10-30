f88cd02dd9f34acfbd2ee51d3a3a076db62bc158
Ulrich <boxcat@boxcat>
2014-10-30 12:04:38 +0100

    Set

--- Aufgabe4/FunSet.java
+++ Aufgabe4/FunSet.java
@@ -1,22 +1,54 @@
+import java.util.ArrayList;
+/**Ein Objekt des Typs FunSet stellt ein Paar Skier inklusive Bindungen und Stöcken,
+ *  ein Snowboard inklusive Bindungen oder ähnliches dar. Je nach Produkt wird bereits 
+ *  bei der Erstellung des Objekts eine maximale Anzahl an Verleihungen festgelegt, 
+ *  die nicht überschritten werden darf. Folgende Methoden sind nur in benutztem 
+ *  Zustand aufrufbar: Die Methode void service() sollte spätestens nach jeder dritten 
+ *  Retournierung aufgerufen werden. Die Methode kontrolle() gibt beeinflusst durch Zufall
+ *   true oder false zurück, aber true darf nur zurückgegeben werden, wenn die maximale 
+ *   Anzahl an Verleihungen noch nicht überschritten und spätestens nach jeweils drei 
+ *   Verleihungen zuvor service() aufgerufen wurde.*/
 public class FunSet extends Set {
 
-  @Override
-  public void verleihe(String kunde) {
+	private int maxVerleihungen;
+	
+	public FunSet(int maxVerleihungen) {
+		super();
+		super.
+		this.maxVerleihungen = maxVerleihungen;
+	}
+	
+	@Override
+	public void verleihe(String kunde) {
+		
+	}
 
-  }
+	@Override
+	public String kunde() {
+		return null;
+	}
 
-  @Override
-  public String kunde() {
-    return null;
-  }
+	@Override
+	public void retour() {
 
-  @Override
-  public void retour() {
+	}
 
-  }
+	@Override
+	public boolean kontrolle() {
+		return false;
+	}
 
-  @Override
-  public boolean kontrolle() {
-    return false;
-  }
+	public ArrayList<Artikel> getArtikelSet() {
+		return null;
+	}
+
+	public void artikelHinzufuegen(Artikel a) {
+		artikelListe.add(a);
+	}
+
+	@Override
+	public Zustand zustand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Aufgabe4/Set.java
+++ Aufgabe4/Set.java
@@ -2,16 +2,63 @@ import java.util.ArrayList;
 
 public abstract class Set implements Artikel {
 
-  private ArrayList<Artikel> artikelSet;
+	private ArrayList<Artikel> artikelListe;
+	private Zustand zustand;
+	private String kunde;
+	
+	public Set() {
+		this.artikelListe = new ArrayList<Artikel>();
+		this.zustand = Zustand.verleihbar;
+	}
+	
+	public ArrayList<Artikel> getArtikelSet() {
+		return this.artikelListe;
+	}
+	
+	public void artikelHinzufuegen(Artikel a) {
+		artikelListe.add(a);
+	}
 
-  @Override
-  public String kunde() {
-    return null;
+	protected void setZustand(Zustand zustand) {
+		this.zustand = zustand;
+	}
+	
+	@Override
+	public Zustand zustand() {
+		return this.zustand;
+	}
+	
+	@Override
+	public void verleihe(String kunde) {
+		if(this.zustand == Zustand.verleihbar) {
+			this.zustand = Zustand.verliehen;
+			this.kunde = kunde;			
+		}
+	}
+
+  public void service() {
+    if (zustand() == Zustand.benutzt) {
+
+    }
   }
 
-  @Override
-  public Zustand zustand() {
-    return null;
-  }
+	@Override
+	public String kunde() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public void retour() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean kontrolle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 }
