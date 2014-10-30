8cbf9bfef9e9b12674c62103780273b87348ec6b
Tom Muhm <tom.muhm@gmail.com>
2014-10-30 13:21:40 +0100

    fixed SchutzBekleidung

--- Aufgabe4/SchutzBekleidung.java
+++ Aufgabe4/SchutzBekleidung.java
@@ -1,17 +1,3 @@
-public abstract class SchutzBekleidung extends Schutz implements KategorieSchutz, KategorieBekleidung {
+public abstract class SchutzBekleidung extends Set implements KategorieSchutz, KategorieBekleidung {
 
-	@Override
-	public void verleihe(String kunde) {
-
-	}
-
-	@Override
-	public void retour() {
-
-	}
-
-	@Override
-	public boolean kontrolle() {
-		return false;
-	}
 }
