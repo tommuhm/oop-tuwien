632c685f8c92aa380fa19e5a9e31724f6179b96f
Tom Muhm <tom.muhm@gmail.com>
2014-11-04 17:05:30 +0100

    repaired subtype-relationship

--- Aufgabe4/SchutzBekleidung.java
+++ Aufgabe4/SchutzBekleidung.java
@@ -1,11 +1,9 @@
-public class SchutzBekleidung extends ArtikelKlasse implements KategorieSchutz, KategorieBekleidung {
+public class SchutzBekleidung extends Schutz implements KategorieSchutz, KategorieBekleidung {
 
-	private int anzVerleihbar;
 	private boolean desfiniziert;
 
 	public SchutzBekleidung(String name, int anzVerleihbar) {
-		super(name);
-		this.anzVerleihbar = anzVerleihbar;
+		super(name, anzVerleihbar);
 		this.desfiniziert = true;
 	}
 
@@ -14,8 +12,4 @@ public class SchutzBekleidung extends ArtikelKlasse implements KategorieSchutz, 
 		desfiniziert = true;
 	}
 
-	@Override
-	public int getAnzVerleihbar() {
-		return anzVerleihbar;
-	}
 }
