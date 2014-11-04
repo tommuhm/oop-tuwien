73bce1927e75040dc90c526a8c350457a140002b
Tom Muhm <tom.muhm@gmail.com>
2014-11-04 17:45:24 +0100

    da hast

--- Aufgabe4/FunSet.java
+++ Aufgabe4/FunSet.java
@@ -2,46 +2,48 @@ public class FunSet extends Set {
 
 	private int maxVerleihungen;
 	private int serviceCount;
-	
-	//Vorbedingung: name darf nicht NULL sein. ServiceCount wird auf 0 gesetzt. 
+
+	// Vorbedingung: name darf nicht NULL sein. ServiceCount wird auf 0 gesetzt.
 	public FunSet(String name, int maxVerleihungen) {
 		super(name);
 		this.maxVerleihungen = maxVerleihungen;
 		this.serviceCount = 0;
 	}
-	
-	//Nachbedingung: ServiceCount wird auf 0 gesetzt.
+
+	// Nachbedingung: ServiceCount wird auf 0 gesetzt.
 	public void service() {
-		if(this.serviceCount <= 3) //Angabe unklar: wissen nicht, was passier, falls der Artikel 4 mal gebraucht wurde, ohne dass ein Service durchgefuehrt worden ist. 
-			this.serviceCount = 0;		 
+		if (this.serviceCount <= 3) // Note: Angabe unklar - wissen nicht was passiert, falls der Artikel 4 mal gebraucht wurde, ohne dass ein Service durchgefuehrt worden ist.
+			this.serviceCount = 0;
 	}
-	
-	//Nachbedingung: Ggstand wird zurueckgegeben, Zustand geht auf verliehen. ServiceCount wird um 1 erhoeht.
+
+	// Nachbedingung: Gegenstand wird zurueckgegeben, Zustand geht auf verliehen. ServiceCount wird um 1 erhoeht.
 	@Override
 	public void retour() {
-		if(zustand() != Zustand.verliehen)
+		if (zustand() != Zustand.verliehen)
 			return;
-		
+
 		super.retour();
 		this.serviceCount++;
 	}
-	//Vorbedingung: Der Zustand des Ggstd muss verleihbar sein, und es muss mindestens ein Gegenstand noch vorhanden sein.
-	//Nachbedingung: Die Anzahl wird um ein Stueck verringert.
+
+	// Vorbedingung: Der Zustand des Ggstd muss verleihbar sein, und es muss mindestens ein Gegenstand noch vorhanden sein.
+	// Nachbedingung: Die Anzahl wird um ein Stueck verringert.
 	@Override
 	public void verleihe(String kunde) {
-		if(zustand() != Zustand.verleihbar && maxVerleihungen > 0)	
+		if (zustand() != Zustand.verleihbar || maxVerleihungen <= 0)
 			return;
-		
+
 		maxVerleihungen--;
-		super.verleihe(kunde);	
+		super.verleihe(kunde);
 	}
-	//Nachbedingung: siehe ArtikelKlasse.kontrolle, zusaetzlich wird geprueft, ob die Artikelanzahl noch ein gueltiger Wert ist und der Ggstd noch in Ordnung ist.
+
+	// Nachbedingung: siehe ArtikelKlasse.kontrolle, zusaetzlich wird geprueft, ob die Artikelanzahl noch ein gueltiger Wert ist und der Ggstd noch in Ordnung ist.
 	@Override
 	public boolean kontrolle() {
-		if(serviceCount >= 4 && maxVerleihungen <= 0)
+		if (serviceCount >= 4 || maxVerleihungen <= 0)
 			return false;
-		
+
 		return super.kontrolle();
 	}
-	
+
 }
