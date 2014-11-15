7729d409dd1d19b49ef1934adf9a98b7f190b554
Tom Muhm <tom.muhm@gmail.com>
2014-11-15 21:29:04 +0100

    implemented wood iterator

--- Aufgabe5/Wood.java
+++ Aufgabe5/Wood.java
@@ -3,6 +3,15 @@ public class Wood<T> {
 
 	private WoodyNode node;
 
+	public Wood(T wert) {
+		this.node = new WoodyNode(this, wert);
+	}
+
+	// TODO - remove class - how to get node from wood without getter? we need it in the iterator
+	public WoodyNode getNode() {
+		return node;
+	}
+
 	public LeveledIter<Wood> contains(Wood wood) {
 		// TODO - gibt iterator ueber alle gleichen elemente zureck (nicht identisch)
 		return null;
@@ -24,23 +33,74 @@ public class Wood<T> {
 
 }
 
+class WoodyNode<T> {
+
+	private Wood<T> wood;
+	private T wert;
+	private Wood<T> sub;
+	private WoodyNode<T> next;
+	private WoodyNode<T> prev;
+
+	public WoodyNode(Wood wood, T wert) {
+		this.wood = wood;
+		this.wert = wert;
+	}
+
+	public Wood<T> getWood() {
+		return wood;
+	}
+
+	public Wood<T> getSub() {
+		return sub;
+	}
+
+	public boolean hasNext() {
+		if (next != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean hasPrev() {
+		if (prev != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public WoodyNode<T> getNext() {
+		return next;
+	}
+
+	public WoodyNode<T> getPrev() {
+		return prev;
+	}
+}
+
 class WoodIter implements LeveledIter<Wood> {
 
 	WoodyNode node = null;
 
 	@Override
 	public LeveledIter<Wood> sub() {
-		if (node != null) {
-			return node.getSub();
+		Wood sub = node.getSub();
+
+		LeveledIter<Wood> subIter;
+		if (sub == null) {
+			subIter = null;
 		} else {
-			return null;
+			subIter = sub.iterator();
 		}
+
+		return subIter;
 	}
 
 	@Override
 	public void add(Wood wood) {
 		if (node == null) {
-			node = new WoodyNode(wood);
+			node = wood.getNode();
 		} else {
 			// TODO
 		}
@@ -63,7 +123,7 @@ class WoodIter implements LeveledIter<Wood> {
 	@Override
 	public Wood next() {
 		if (hasNext()) {
-			return node.getNext();
+			return node.getNext().getWood();
 		} else {
 			return null;
 		}
@@ -81,10 +141,11 @@ class WoodIter implements LeveledIter<Wood> {
 	@Override
 	public Wood pervious() {
 		if (hasPrevious()) {
-			return node.getPrev();
+			return node.getPrev().getWood();
 		} else {
 			return null;
 		}
 	}
 
 }
+
