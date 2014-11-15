4b111c3d429fa55aa38f562335e38f43e3172658
Tom Muhm <tom.muhm@gmail.com>
2014-11-15 20:30:38 +0100

    created inner iterator class

--- Aufgabe5/Wood.java
+++ Aufgabe5/Wood.java
@@ -1,14 +1,60 @@
 
 public class Wood<T> {
 
+	private Wood<T> prev;
+	private Wood<T> next;
+	private Wood<T> sub;
+
 	public LeveledIter<T> contains(T e) {
 		// TODO - gibt iterator ueber alle gleichen elemente zureck (nicht identisch)
 		return null;
 	}
 
 	public LeveledIter<T> iterator() {
+		WoodIter<Wood> rootIterator = new WoodIter<Wood>();
+		rootIterator.add(this);
+
 		// TODO - gibt die wurzeln aller baeume zurueck
 		return null;
 	}
 
+
+}
+
+class WoodIter<Wood> implements LeveledIter {
+
+	@Override
+	public LeveledIter<Wood> sub() {
+		return null;
+	}
+
+	@Override
+	public void add(Object o) {
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		return false;
+	}
+
+	@Override
+	public Wood next() {
+		return null;
+	}
+
+	@Override
+	public void remove() {
+
+	}
+
+	@Override
+	public Wood pervious() {
+		return null;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		return false;
+	}
 }
