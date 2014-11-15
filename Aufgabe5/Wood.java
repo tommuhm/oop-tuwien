9a160eb65053f0838a4ff66cac286d5fd8956768
Tom Muhm <tom.muhm@gmail.com>
2014-11-15 19:47:24 +0100

    added sub, add, remove methods

--- Aufgabe5/Wood.java
+++ Aufgabe5/Wood.java
@@ -1,4 +1,14 @@
 
 public class Wood<T> {
 
+	public LeveledIter<T> contains(T e) {
+		// TODO - gibt iterator ueber alle gleichen elemente zureck (nicht identisch)
+		return null;
+	}
+
+	public LeveledIter<T> iterator() {
+		// TODO - gibt die wurzeln aller baeume zurueck
+		return null;
+	}
+
 }
