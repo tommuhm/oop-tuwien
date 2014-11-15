c277efecf646b84a4b093b7ca0179e2fc75bb36e
Tom Muhm <tom.muhm@gmail.com>
2014-11-15 20:30:17 +0100

    started first testcase

--- Aufgabe5/Test.java
+++ Aufgabe5/Test.java
@@ -4,6 +4,13 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		Wood<String> woodString = new Wood<String>();
+		SortedWood<Skier> woodSkier = new SortedWood<Skier>();
+		SortedWood<Snowboarder> woodSnowboarder = new SortedWood<Snowboarder>();
+
+
 	}
+
 
 }
