c935182d5ea7df1f7d5eb7dc82b2f83de0f5479b
djaffry <dave.jaffry@gmail.com>
2014-11-18 21:57:02 +0100

    nachbedingungen und vorbedingungen

--- Aufgabe5/Prec.java
+++ Aufgabe5/Prec.java
@@ -1,6 +1,8 @@
 
 public interface Prec<E> {
 
+	//Vorbedingung: x darf nicht null sein
+	//Nachbedingung: vergleicht zwei Objekte zusammenpassender Typen miteinander
 	public boolean prec(E x);
 
 }
