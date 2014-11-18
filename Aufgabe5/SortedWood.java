6a5ee0ec8fbf2f6cf85fa3595cc0c0a41a473f7d
Tom Muhm <tom.muhm@gmail.com>
2014-11-18 13:30:19 +0100

    init sortedWoord

--- Aufgabe5/SortedWood.java
+++ Aufgabe5/SortedWood.java
@@ -1,4 +1,19 @@
 
-public class SortedWood<T> extends Wood<T> {
+public class SortedWood<T extends Prec> extends Wood<T> implements Prec {
 
+	public SortedWood(T wert) {
+		super(wert);
+		// TODO
+	}
+
+	@Override
+	public boolean prec(Object x) {
+		// TODO
+		return false;
+	}
+
+	public LeveledIter<T> sorted() {
+		// TODO
+		return null;
+	}
 }
