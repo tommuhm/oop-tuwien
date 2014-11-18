834985545a442492278b1182f5b14f48383ff896
djaffry <dave.jaffry@gmail.com>
2014-11-18 21:30:30 +0100

    added nachbedingungen und zusicherungen

--- Aufgabe5/Iter.java
+++ Aufgabe5/Iter.java
@@ -2,9 +2,11 @@ import java.util.Iterator;
 
 
 public interface Iter<E> extends Iterator<E> {
-
+	
+	//Nachbedingung: gibt an, ob es noch ein vorheriges Element gibt
 	boolean hasPrevious();
 
+	//Nachbedingung: wechselt zum vorherigen Element
 	public E previous();
 
 }
--- Aufgabe5/LeveledIter.java
+++ Aufgabe5/LeveledIter.java
@@ -1,10 +1,13 @@
 
 public interface LeveledIter<E> extends Iter<E> {
 
+	//Nachbedingung: wechselt in die Substruktur vom derzeitigen Wood
 	public LeveledIter<E> sub();
 
+	//Nachbedingung: fuegt ein neues Element zum derzeitigen Wood hinzu
 	public void add(E e);
 
+	//Nachbedingung: entfernt alle Elemente vom derzeitigen Wood
 	public void remove();
 
 }
