1b55df776e7cf8f860f0c6f10740d2708926be81
djaffry <dave.jaffry@gmail.com>
2014-11-22 17:59:25 +0100

    added some nachbedingungen

--- Aufgabe6/Raumsonde.java
+++ Aufgabe6/Raumsonde.java
@@ -1,14 +1,13 @@
-import java.util.HashMap;
-
-
 public class Raumsonde {
 
 	private int maxNutzlast;
 	
+	//Vorbedingung: maxNutzlast muss groesser als null sein
 	public Raumsonde(int maxNutzlast) {
 		this.maxNutzlast = maxNutzlast;		
 	}
 
+	//Nachbedingung: gibt die maximale Nutzlast zurueck
 	public int getMaxNutzlast() {
 		return maxNutzlast;
 	}
