f4c6067d62dcb50d574d7df43bf71da279c8a418
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-11-22 12:56:24 +0100

    started RaumSonde

--- Aufgabe6/RaumSonde.java
+++ Aufgabe6/RaumSonde.java
@@ -1,4 +1,34 @@
+import java.util.HashMap;
+
 
 public class RaumSonde {
 
+	//HashMap<String, Planet> himmelskoerper;
+	
+	public RaumSonde() {
+		
+		
+	}
+	
+	
+	public Modul add(Planet planet) {
+		//TODO implement add.
+		return null;
+	}
+	
+	
+	public Planet remove (String name) {
+		//TODO implement remove.
+		return null;
+	}
+
+	public void missionslist() {
+		//TODO implement
+		return;
+	}
+
+	public void utilization() {
+		//TODO implement
+		return;
+	}
 }
