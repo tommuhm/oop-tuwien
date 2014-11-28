34ed66bf8af3134d64f3eec2b9d6e3c4d2c81e29
Tom Muhm <tom.muhm@gmail.com>
2014-11-28 16:36:05 +0100

    implemented move

--- Aufgabe7/Ameise.java
+++ Aufgabe7/Ameise.java
@@ -9,22 +9,58 @@ public class Ameise extends Thread {
 	private Kammer ameisenkolonie;
 	private Strategie strategie;
 
+	private int dosis;
+	private Kammer curKammer;
+
 	public Ameise(Kammer[][] labyrinth, Kammer ameisenkolonie, Strategie strategie) {
 		super(null, null, "Ameise-" + ameiseNr++, stackSize);
 		this.labyrinth = labyrinth;
 		this.ameisenkolonie = ameisenkolonie;
 		this.strategie = strategie;
+		this.curKammer = ameisenkolonie;
+		// TODO
+		this.dosis = labyrinth.length * labyrinth[0].length;
 	}
 
 	public void run() {
+
+		try {
+
+			while (dosis > 0) {
+				pause();
+
+				move();
+
+
+			}
+
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException " + e.getMessage());
+//			e.printStackTrace();
+		}
+
+
 		System.out.println("Test");
 	}
 
 
-	public void pause() throws InterruptedException {
+	private void pause() throws InterruptedException {
 		Random rand = new Random();
 		int waitTime = rand.nextInt(15)+5;
 		this.sleep(waitTime);
+	}
+
+	private void move() {
+		boolean moved = false;
+		while (!moved) {
+			Kammer kammer = strategie.naechsteKammer();
+			moved = kammer.addAmeise(this);
+			if (moved) {
+				dosis--;
+				curKammer.removeAmeise();
+				curKammer = kammer;
+			}
+		}
 	}
 
 }
