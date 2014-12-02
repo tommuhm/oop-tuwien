018d0d8f70491519277762c4b557e8543501274f
Tom Muhm <tom.muhm@gmail.com>
2014-12-02 21:23:42 +0100

    autoformat, removed unneeded sys outs

--- Aufgabe7/Controller.java
+++ Aufgabe7/Controller.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 
 public class Controller {
-	
+
 	private Kammer[][] labyrinth;
 	private Kammer ameisenkolonie;
 	private Kammer futterstelle;
 	private ArrayList<Ameise> ameisen;
 	private int groesse;
-	
+
 	public Controller(Kammer[][] labyrinth, Kammer ameisenkolonie, Kammer futterstelle) {
 		this.labyrinth = labyrinth;
 		this.ameisenkolonie = ameisenkolonie;
@@ -16,84 +16,76 @@ public class Controller {
 		this.groesse = labyrinth.length * labyrinth[0].length; //Labyrinth != null!!!!!!
 		this.ameisen = new ArrayList<Ameise>();
 	}
-	
+
 	public void start() {
 
-		Ameise leitameise = new Leitameise(labyrinth, ameisenkolonie, new StrategieRandom(), groesse*2);
+		Ameise leitameise = new Leitameise(labyrinth, ameisenkolonie, new StrategieRandom(), groesse * 2);
 		ameisenkolonie.addAmeise(leitameise);
 		//ameisen.add(leitameise); // TODO ?
 
 		synchronized (leitameise) {
 			leitameise.start();
 
-			while(true) {
+			while (true) {
 				try {
-						if(leitameise.getState() == Thread.State.TERMINATED) {
-							
-							leitameise.interrupt();
-							System.out.println(leitameise.toString());
-							for(Ameise ameise : ameisen) {
-								ameise.interrupt();
-								System.out.println(ameise.toString());
-							}
-							return;
-						} else {
-							leitameise.wait();
-						
-							System.out.println(ameisen.size() + " < " + (groesse/10) + " = " + (ameisen.size() < (groesse/10)));
-							if(ameisen.size() < (groesse/10)) {
-								Ameise neueAmeise = new Ameise(this.labyrinth, this.ameisenkolonie, Strategie.getNextStrategie());
-								if(ameisenkolonie.addAmeise(neueAmeise)) {
-									System.out.println("Neue Ameise hinzugefügt.");
-								} else {
-									System.out.println("Neue Ameise NICHT hinzugefügt.");
-								}	
-								
-								ameisen.add(neueAmeise);
-								neueAmeise.start();
-							}
-							
-							System.out.println("Ameise hat genotified. >:^|");
-							this.printLabyrinth();
-							leitameise.notifyAll();
+					if (leitameise.getState() == Thread.State.TERMINATED) {
+
+						leitameise.interrupt();
+						System.out.println(leitameise.toString());
+						for (Ameise ameise : ameisen) {
+							ameise.interrupt();
+							System.out.println(ameise.toString());
 						}
-						
+						return;
+					} else {
+						leitameise.wait();
+
+						if (ameisen.size() < (groesse / 10)) {
+							Ameise neueAmeise = new Ameise(this.labyrinth, this.ameisenkolonie, Strategie.getNextStrategie());
+							ameisen.add(neueAmeise);
+							neueAmeise.start();
+						}
+
+						this.printLabyrinth();
+						leitameise.notifyAll();
+					}
+
 				} catch (InterruptedException e) {
 					System.err.println("Im Controller wurde eine Exception geworfen.");
 					e.printStackTrace();
-				};
+				}
 			}
 		}
 	}
-	
+
 	public synchronized void printLabyrinth() {
-	//synchronized (labyrinth) {
+		//synchronized (labyrinth) {
 		int i = 0;
-		for(int y = 0; y < labyrinth.length; y++) {
+		for (int y = 0; y < labyrinth.length; y++) {
 			//synchronized (labyrinth[y]) {
-				for(int x = 0; x < labyrinth[y].length; x++) {
-					if(y != 0) {
-						if(labyrinth[y][x].isMauerOben()) {
-							System.out.print("--");
-						} else {
-							System.out.print("  ");
-						}
-					}
-				}
-				System.out.println();
-				
-				for(int x = 0; x < labyrinth[y].length; x++) {
-					System.out.print(labyrinth[y][x].getDosis());
-					if(labyrinth[y][x].isMauerRechts()) {
-						System.out.print("|");
+			for (int x = 0; x < labyrinth[y].length; x++) {
+				if (y != 0) {
+					if (labyrinth[y][x].isMauerOben()) {
+						System.out.print("--");
 					} else {
-						System.out.print(" ");
+						System.out.print("  ");
 					}
 				}
-				System.out.println();
+			}
+			System.out.println();
+
+			for (int x = 0; x < labyrinth[y].length; x++) {
+				System.out.print(labyrinth[y][x].getDosis());
+				if (labyrinth[y][x].isMauerRechts()) {
+					System.out.print("|");
+				} else {
+					System.out.print(" ");
 				}
-			}				
-		//}
+			}
+			System.out.println();
+		}
+	}
 	//}
-	
+	//}
+
 }
