5a50f76e6c27994accd8e335def417a94de7bc7f
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-02 23:30:36 +0100

    added spaces.

--- Aufgabe7/Feld.java
+++ Aufgabe7/Feld.java
@@ -9,10 +9,10 @@ public class Feld {
 	private AtomicInteger ameisencounter;
 	private boolean mauerOben;
 	private boolean mauerRechts;
-	//Invariante: x muss zwischen 0 und der Breite von Feld[][] sein.
-	//Invariante: y muss zwischen 0 und der Höhe von Feld[][] sein.
-	//Invariante: dosis darf nicht negativ werden.
-	//Invariante: ameisencounter muss zwischen 0 und 2 sein.
+	// Invariante: x muss zwischen 0 und der Breite von Feld[][] sein.
+	// Invariante: y muss zwischen 0 und der Höhe von Feld[][] sein.
+	// Invariante: dosis darf nicht negativ werden.
+	// Invariante: ameisencounter muss zwischen 0 und 2 sein.
 
 	public Feld(int x, int y) {
 		this.x = x;
@@ -33,10 +33,10 @@ public class Feld {
 		this.mauerRechts = mauerRechts;
 	}
 
-	//Vorbedingung: Dosis ist true wenn eine Dosis hinzugefügt werden soll; sonst false.
-	//Nachbedingung: ameisencounter wird um 1 erhöht falls ameisencounter 0 oder 1 ist.
-	//Nachbedingung: dosis wird um 1 erhöht falls ameisencounter erhöht wurde
-	//Nachbedingung: Gibt true zurück falls dosis und ameisencounter erhöht wurden, sonst false.
+	// Vorbedingung: Dosis ist true wenn eine Dosis hinzugefügt werden soll; sonst false.
+	// Nachbedingung: ameisencounter wird um 1 erhöht falls ameisencounter 0 oder 1 ist.
+	// Nachbedingung: dosis wird um 1 erhöht falls ameisencounter erhöht wurde
+	// Nachbedingung: Gibt true zurück falls dosis und ameisencounter erhöht wurden, sonst false.
 	public synchronized boolean addAmeise(boolean dosis) { //TODO addDosis.
 		if (this.ameisencounter.get() < 2) {
 			this.ameisencounter.incrementAndGet();
@@ -48,8 +48,8 @@ public class Feld {
 		return false;
 	}
 
-	//Nachbedingung: ameisencounter wird um 1 verringert, sollte ameisencounter 1 oder 2 sein.
-	//Nachbedingung: Gibt true zurück falls ameisencounter verringert wurde, sonst false.
+	// Nachbedingung: ameisencounter wird um 1 verringert, sollte ameisencounter 1 oder 2 sein.
+	// Nachbedingung: Gibt true zurück falls ameisencounter verringert wurde, sonst false.
 	public synchronized boolean removeAmeise() {
 		if (this.ameisencounter.get() > 0) {
 			this.ameisencounter.decrementAndGet();
@@ -58,17 +58,17 @@ public class Feld {
 		return false;
 	}
 
-	//Nachbedingung: Gibt true zurück wenn ameisencounter kleiner als 2 ist, sonst false.
+	// Nachbedingung: Gibt true zurück wenn ameisencounter kleiner als 2 ist, sonst false.
 	public boolean hatPlatz() {
 		return ameisencounter.intValue() < 2;
 	}
 
-	//Nachbedingung: Gibt mauerOben zurück.
+	// Nachbedingung: Gibt mauerOben zurück.
 	public boolean hatMauerOben() {
 		return mauerOben;
 	}
 
-	//Nachbedingung: Gibt mauerRechts zurück.
+	// Nachbedingung: Gibt mauerRechts zurück.
 	public boolean hatMauerRechts() {
 		return mauerRechts;
 	}
