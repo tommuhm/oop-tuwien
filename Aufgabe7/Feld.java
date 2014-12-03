6e7f9ec4dfb1153d7da1a21db4b492a63e9d06d3
Tom Muhm <tom.muhm@gmail.com>
2014-12-03 03:36:00 +0100

    umlaute -.-

--- Aufgabe7/Feld.java
+++ Aufgabe7/Feld.java
@@ -12,7 +12,7 @@ public class Feld {
 	private boolean mauerRechts;
 	private ReentrantLock lock;
 	// Invariante: x muss zwischen 0 und der Breite von Feld[][] sein.
-	// Invariante: y muss zwischen 0 und der Höhe von Feld[][] sein.
+	// Invariante: y muss zwischen 0 und der Hoehe von Feld[][] sein.
 	// Invariante: dosis darf nicht negativ werden.
 	// Invariante: ameisencounter muss zwischen 0 und 2 sein.
 
@@ -40,9 +40,9 @@ public class Feld {
 
 	// Vorbedingung: Dosis ist true wenn eine Dosis hinzugefuegt werden soll; sonst false.
 	// Nachbedingung: Gibt false zurueck wenn der Feld gelocket ist
-	// Nachbedingung: ameisencounter wird um 1 erhöht falls ameisencounter 0 oder 1 ist.
-	// Nachbedingung: dosis wird um 1 erhöht falls ameisencounter erhöht wurde
-	// Nachbedingung: Gibt true zurueck falls ameisencounter erhöht wurde, sonst false.
+	// Nachbedingung: ameisencounter wird um 1 erhoeht falls ameisencounter 0 oder 1 ist.
+	// Nachbedingung: dosis wird um 1 erhoeht falls ameisencounter erhoeht wurde
+	// Nachbedingung: Gibt true zurueck falls ameisencounter erhoeht wurde, sonst false.
 	public synchronized boolean addAmeise(boolean dosis) {
 		if (!lock.isLocked()) {
 			if (this.hatPlatz()) {
