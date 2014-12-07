fddc1755108f9b69404f7b9d3a7651c3bf668445
Tom Muhm <tom.muhm@gmail.com>
2014-12-07 12:56:36 +0100

    implemented mission

--- Aufgabe8/Mission.java
+++ Aufgabe8/Mission.java
@@ -2,9 +2,26 @@
 @Author(name = "Thomas Muhm")
 public class Mission {
 
-	public Mission() {
+	private final String name;
+	private MyLinkedList<Raumsonde> raumsonden;
 
+	public Mission(String name) {
+		this.name = name;
+		this.raumsonden = new MyLinkedList<Raumsonde>();
 	}
 
+	public void addRaumsonde(Raumsonde raumsonde) {
+		raumsonden.add(raumsonde);
+	}
+
+	public boolean removeRaumsonde(Raumsonde raumsonde) {
+		return raumsonden.remove(raumsonde);
+	}
+
+	public void printRaumstonden() {
+		for (int i = 0; i < raumsonden.size(); i++) {
+			System.out.println(raumsonden.get(i));
+		}
+	}
 
 }
