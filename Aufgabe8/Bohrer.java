981fd4c7ccb5d3613914388fcea7ad164b1817aa
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-09 18:15:22 +0100

    Added Conditions.

--- Aufgabe8/Bohrer.java
+++ Aufgabe8/Bohrer.java
@@ -1,16 +1,21 @@
 
 @Author(name="Ulrich Aschl")
 public class Bohrer implements Einsatzart {
-	private double laenge;
 	
+	private double laenge;
+	// Invariante: laenge darf nicht kleiner 0 sein.
+	
+	// Vorbedingung: laenge darf nicht kleiner 0 sein.
 	public Bohrer(double laenge) {
 		this.laenge = laenge;
 	}
 	
+	// Nachbedingung: gibt die Länge des Bohrers zurück.
 	public double getLaenge() {
 		return this.laenge;
 	}
 	
+	// Nachbedingung: Gibt die Länge des Bohrers als String aus.
 	@Override
 	public String toString() {
 		return "Bohrer: " + this.laenge + " cm";
--- Aufgabe8/Kamera.java
+++ Aufgabe8/Kamera.java
@@ -1,16 +1,21 @@
 
 @Author(name="Ulrich Aschl")
 public class Kamera implements Einsatzart{
-	private int pixel;
 	
+	private int pixel;
+	// Invariante: pixel darf nicht kleiner 0 sein.
+
+	// Vorbedingung: pixel darf nicht kleiner 0 sein.
 	public Kamera(int pixel) {
 		this.pixel = pixel;
 	}
-	
+
+	// Nachbedingung: gibt die Anzahl an Pixel der Kamera zurück.
 	public int getPixel() {
 		return this.pixel;
 	}
-	
+
+	// Nachbedingung: Gibt die Anzahl an Pixel der Kamera als String aus.
 	@Override
 	public String toString() {
 		return "Kamera: " + this.pixel + " Pixel";
