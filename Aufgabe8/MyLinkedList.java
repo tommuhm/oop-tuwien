b9ac3202ce51fc42c7078ccd521d45de45e004f0
Tom Muhm <tom.muhm@gmail.com>
2014-12-09 17:05:44 +0100

    updated linkedList

--- Aufgabe8/MyLinkedList.java
+++ Aufgabe8/MyLinkedList.java
@@ -1,51 +1,69 @@
 @Author(name = "Thomas Muhm")
-public class MyLinkedList<A> {
+public class MyLinkedList {
 
 	private Node head;
 	private int size;
 
+	@Author(name = "Thomas Muhm")
 	public MyLinkedList() {
-		head = new Node(null);
+		head = new Node(null, null);
 		size = 0;
 	}
 
-	public void add(A elem) {
-		Node node = new Node(elem);
-		Node current = head;
-		while (current.next() != null) {
-			current = current.next();
-		}
-		node.setNext(current.next());
-		current.setNext(node);
-		size++;
-	}
-
-	public boolean remove(A elem) {
-		Node current = head;
+	@Author(name = "Thomas Muhm")
+	public boolean add(Object id, Object elem) {
+		Node node = new Node(id, elem);
 		Node prev = null;
-		while (!elem.equals(current.getElem()) && current.next() != null) {
+		Node current = head;
+		while (current != null) {
+			if (current.id.equals(id)) {
+				return false;
+			}
 			prev = current;
 			current = current.next();
 		}
-		if (elem.equals(current.getElem())) {
-			if (prev == null) {
-				head = current.next();
-			} else {
-				prev.setNext(current.next());
-			}
-			size--;
-			return true;
+
+		node.setNext(current.next());
+		if (prev != null) {
+			prev.setNext(node);
 		} else {
-			return false;
+			head = node;
 		}
 
+		size++;
+		return true;
 	}
 
+	@Author(name = "Thomas Muhm")
+	public boolean removeById(Object id) {
+		Node current = head;
+		Node prev = null;
+
+		while (current != null) {
+			if (id.equals(current.id)) {
+				if (prev == null) {
+					head = current.next();
+				} else {
+					prev.setNext(current.next());
+				}
+				size--;
+				return true;
+			} else {
+				prev = current;
+				current = current.next();
+			}
+		}
+
+		return false;
+	}
+
+	@Author(name = "Thomas Muhm")
 	public int size() {
 		return size;
 	}
 
-	public A get(int index) {
+	@Author(name = "Thomas Muhm")
+	public Object get(int index) {
 		if (index < 0 || index >= size) {
 			return null;
 		}
@@ -58,24 +76,43 @@ public class MyLinkedList<A> {
 		return current.getElem();
 	}
 
+	@Author(name = "Thomas Muhm")
+	public Object getById(Object id) {
+		Node current = head;
+		while (current != null) {
+			if (current.id.equals(id)) {
+				return current;
+			}
+			current = current.next();
+		}
+		return null;
+	}
+
+	@Author(name = "Thomas Muhm")
 	private class Node {
 
 		Node next;
-		A elem;
+		Object id;
+		Object elem;
 
-		public Node(A elem) {
+		@Author(name = "Thomas Muhm")
+		public Node(Object id, Object elem) {
+			this.id = id;
 			this.elem = elem;
 			next = null;
 		}
 
-		public A getElem() {
+		@Author(name = "Thomas Muhm")
+		public Object getElem() {
 			return elem;
 		}
 
+		@Author(name = "Thomas Muhm")
 		public Node next() {
 			return next;
 		}
 
+		@Author(name = "Thomas Muhm")
 		private void setNext(Node next) {
 			this.next = next;
 		}
