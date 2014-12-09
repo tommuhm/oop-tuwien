dd5d95dc37f378c7a33dcd4211a34c41382b8e0d
Tom Muhm <tom.muhm@gmail.com>
2014-12-09 19:04:19 +0100

    removed toString

--- Aufgabe8/Einsatzart.java
+++ Aufgabe8/Einsatzart.java
@@ -2,7 +2,4 @@
 @Author(name = "Ulrich Aschl")
 public interface Einsatzart {
 
-	@Author(name = "Ulrich Aschl")
-	// Nachbedingung: gibt eine String-Representation der Einsatzart zurueck
-	public String toString();
 }
