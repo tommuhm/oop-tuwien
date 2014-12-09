0d5a4b4b9f87ae747ec584da316edc08e694bf9a
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-09 16:37:07 +0100

    meh

--- Aufgabe8/Test.java
+++ Aufgabe8/Test.java
@@ -1,8 +1,99 @@
+import java.lang.annotation.Annotation;
+
 
 public class Test {
 
+	private static Raumsonde raum1, raum2, raum3;
+	private static Einsatzart art1, art2, art3, art4, art5;
+	private static Erkundungsroboter r1, r2, r3, r4, r5;
+	private static Mission m1;
+	
 	@Author(name="Ulrich Aschl")
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		init();
+		
+		/*
+		 * • Erstellen und ändern Sie mehrere Missionen mit mehreren Raumsonden mit jeweils einigen
+		 *	Erkundungsrobotern. Jede Raumsonde einer Mission soll über ihren eindeutigen Namen
+		 *	angesprochen werden, und jeder Erkundungsroboter einer Raumsonde über seine eindeutige
+		 *	Nummer.
+		 *	• Fügen Sie zu Missionen einzelne Raumsonden hinzu, entfernen Sie einzelne Raumsonden,
+		 *	wobei Sie Raumsonden nur über deren Namen ansprechen.
+		 */
+		testA();
+		
+		/*
+		 * Fügen Sie zu einigen Raumsonden einzelne Erkundungsroboter hinzu, entfernen Sie
+		 *	einzelne Erkundungsroboter, und ändern Sie die Informationen zu einzelnen
+		 *	Erkundungsrobotern, wobei Sie Erkundungsroboter und Raumsonden nur über deren
+		 *	Nummern und Namen ansprechen.
+		 */
+		testB();
+		
+	}
+	
+	private static void testB() {
+		System.out.println("\n~~~ Test B: ~~~");
+		System.out.println("Erstelle Erkundungsroboter rB1.");
+		Erkundungsroboter rB1 = new SprungRoboter(art1, 30, 10);
+		System.out.println("Füge rB1 zu raum1 hinzu.");
+		raum1.addRoboter(rB1);
+	}
+
+	private static void init() {
+		raum1 = new Raumsonde("raum1");
+		raum2 = new Raumsonde("raum2");
+		raum3 = new Raumsonde("raum3");
+		
+		art1 = new Bohrer(82.28);
+		art2 = new Bohrer(82.28);
+		art3 = new Kamera(200000);		
+		art4 = new Kamera(300000);	
+		art5 = new Kamera(10000);
+		
+		r1 = new SprungRoboter(art1, 15, 20);
+		r2 = new SprungRoboter(art2, 10, 10);
+		r3 = new RadRoboter(art3, 20, 10.20);
+		r4 = new RadRoboter(art4, 30, 5.50);
+		r5 = new RadRoboter(art5, 20, 5);
+		
+		m1 = new Mission("Mission 1");
+	}
+	
+	private static void printAlleStatistiken(Raumsonde r) {
+		System.out.println(r.getBetriebsstundenEinsatzartSchnitt());
+		System.out.println(r.getBetriebsstundenBauartSchnitt());
+		System.out.println(r.getWegstreckeSchnitt());
+		System.out.println(r.getSpruengeSchnitt());
+		System.out.println(r.getMinMaxPixel());
+		System.out.println(r.getBohrerSchnitt());
+	}
+	
+	private static void testA() {
+		System.out.println("~~~ Test A: ~~~");
+		
+		raum1.addRoboter(r1);
+		raum1.addRoboter(r2);
+		raum1.addRoboter(r3);
+		raum2.addRoboter(r4);
+		raum3.addRoboter(r5);
+		
+		Mission m1 = new Mission("Mission 1");
+		
+		m1.addRaumsonde(raum1);
+		m1.addRaumsonde(raum2);
+		
+		System.out.println("Mission 1:");
+		m1.printRaumstonden();
+		System.out.println("Mission 1 - Füge Raumsonde hinzu.");
+		m1.addRaumsonde(raum3);
+		m1.printRaumstonden();
+		System.out.println("Mission 1 - Remove Raumsonde.");
+		m1.removeRaumsonde(raum3);
+		m1.printRaumstonden();		
+	}
+	
+	private static void test1() {
 
 		Einsatzart art1 = new Bohrer(15.92);
 		Einsatzart art2 = new Bohrer(82.28);
@@ -13,14 +104,14 @@ public class Test {
 		Einsatzart art7 = new Kamera(400000);		
 		Einsatzart art8 = new Kamera(400001);		
 		
-		Erkundungsroboter r1 = new SprungRoboter("r1", art1);
-		Erkundungsroboter r2 = new SprungRoboter("r2", 10, 3, art2);
-		Erkundungsroboter r3 = new SprungRoboter("r3", 15, 20, art3);
-		Erkundungsroboter r4 = new SprungRoboter("r4", 10, 0, art4);
-		Erkundungsroboter r5 = new RadRoboter("r5", 20, 10, art5);
-		Erkundungsroboter r6 = new RadRoboter("r6", 30.50, 10, art6);
-		Erkundungsroboter r7 = new RadRoboter("r7", 20.12, 4, art7);
-		Erkundungsroboter r8 = new RadRoboter("r8", art8);
+		Erkundungsroboter r1 = new SprungRoboter(art1);
+		Erkundungsroboter r2 = new SprungRoboter(art2, 10, 3);
+		Erkundungsroboter r3 = new SprungRoboter(art3, 15, 20);
+		Erkundungsroboter r4 = new SprungRoboter(art4, 10, 0);
+		Erkundungsroboter r5 = new RadRoboter(art5, 20, 10);
+		Erkundungsroboter r6 = new RadRoboter(art6, 10, 5.50);
+		Erkundungsroboter r7 = new RadRoboter(art7, 20, 10.90);
+		Erkundungsroboter r8 = new RadRoboter(art8);
 		
 		// Test 1:
 		System.out.println("Roboter r1 wird ausgegeben.");
@@ -76,15 +167,5 @@ public class Test {
 		m1.removeRaumsonde(raum1);
 		System.out.println("Mission 1 - Raumsonde entfernt.");
 		m1.printRaumstonden();
-		
-	}
-	
-	private static void printAlleStatistiken(Raumsonde r) {
-		System.out.println(r.getBetriebsstundenEinsatzartSchnitt());
-		System.out.println(r.getBetriebsstundenBauartSchnitt());
-		System.out.println(r.getWegstreckeSchnitt());
-		System.out.println(r.getSpruengeSchnitt());
-		System.out.println(r.getMinMaxPixel());
-		System.out.println(r.getBohrerSchnitt());
 	}
 }
