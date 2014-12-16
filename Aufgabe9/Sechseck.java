797276d3f2cb826a453c8253c16a63ee049b619b
Tom Muhm <tom.muhm@gmail.com>
2014-12-16 17:57:24 +0100

    implemented kannBehinhalten

--- Aufgabe9/Sechseck.java
+++ Aufgabe9/Sechseck.java
@@ -9,6 +9,21 @@ public class Sechseck extends Grundflaeche {
 
 	@Override
 	public boolean kannBeinhalten(Grundflaeche grundflaeche) {
+		if (grundflaeche instanceof Rechteck || grundflaeche instanceof Quadrat)
+			return false;
+
+		if (grundflaeche instanceof Sechseck) {
+			Sechseck sechseck = (Sechseck) grundflaeche;
+			if (sechseck.getSeitenlaenge() <= (this.seitenlaenge / 2))
+				return true;
+		}
+
+		if (grundflaeche instanceof Kreis ) {
+			Kreis kreis = (Kreis) grundflaeche;
+			if (kreis.getDurchmesser() <= (this.seitenlaenge / 2))
+				return true;
+		}
+
 		return false;
 	}
 
