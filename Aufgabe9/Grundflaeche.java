cca2e6037280ea2f4c373f3610fce787fbe131c3
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-16 21:41:40 +0100

    Added conditions.

--- Aufgabe9/Grundflaeche.java
+++ Aufgabe9/Grundflaeche.java
@@ -1,20 +1,33 @@
 
 public abstract class Grundflaeche {
 
+	// Nachbedingung: gibt die flaeche der Grundflaeche zurück
 	public double flaeche() {
 		return flaeche(0);
 	}
 
+	// Vorbedingung: grundflaeche darf nicht null sein
+	// Nachbedingung: Gibt true zurueck wenn grundflaeche in this passt, sonst false.
 	public abstract boolean kannBeinhalten(Grundflaeche grundflaeche);
 
+	// Vorbedingung: quadrat darf nicht null sein
+	// Nachbedingung: Gibt true zurueck wenn this in quadrat passt, sonst false.
 	public abstract boolean passtIn(Quadrat quadrat);
 
+	// Vorbedingung: rechteck darf nicht null sein
+	// Nachbedingung: Gibt true zurueck wenn this in rechteck passt, sonst false.
 	public abstract boolean passtIn(Rechteck rechteck);
 
+	// Vorbedingung: kreis darf nicht null sein
+	// Nachbedingung: Gibt true zurueck wenn this in kreis passt, sonst false.
 	public abstract boolean passtIn(Kreis kreis);
 
+	// Vorbedingung: sechseck darf nicht null sein
+	// Nachbedingung: Gibt true zurueck wenn this in sechseck passt, sonst false.
 	public abstract boolean passtIn(Sechseck sechseck);
 
+	// Vorbedingung: kartondicke darf nicht kleiner 0 sein
+	// Nachbedingung: gibt die berechnete Flaeche von this zurueck. Die kartondicke kann einbezogen werden.
 	public abstract double flaeche(double kartondicke);
 
 }
--- Aufgabe9/Rechteck.java
+++ Aufgabe9/Rechteck.java
@@ -4,24 +4,33 @@ public class Rechteck extends Grundflaeche {
 	private double laenge;
 	private double breite;
 
-	//invariante für laenge > breite setzen!
+	// Invariante: laenge muss groesser 0 sein.
+	// Invariante: breite muss groesser 0 sein.
+	// Invariante: laenge muss groesser breite sein.
 
+	// Nachbedingung: laenge ist zugewiesen.
+	// Nachbedingung: breite ist zugewiesen.
 	public Rechteck(double laenge, double breite) {
 		this.laenge = laenge;
 		this.breite = breite;
 	}
 
 	@Override
+	// Vorbedingung: grundflaeche darf nicht null sein.
+	// Nachbedingung: gibt true zurueck wenn this das Objekt grundflaeche beinhalten kann.
 	public boolean kannBeinhalten(Grundflaeche grundflaeche) {
 		return grundflaeche.passtIn(this);
 	}
 
 	@Override
+	// Nachbedingung: Rechtecke dürfen keine Quadrate aufnehmen -> false. 
 	public boolean passtIn(Quadrat quadrat) {
 		return false;
 	}
 
 	@Override
+	// Vorbedingung: rechteck darf nicht null sein.
+	// Nachbedingung: Gibt true zurueck wenn this in rechteck passt, sonst false.
 	public boolean passtIn(Rechteck rechteck) {
 		if (this.getLaenge() >= rechteck.getLaenge() && this.getBreite() >= rechteck.getBreite())
 			return true;
@@ -29,11 +38,14 @@ public class Rechteck extends Grundflaeche {
 	}
 
 	@Override
+	// Nachbedingung: Rechtecke dürfen keine Kreise aufnehmen -> false. 
 	public boolean passtIn(Kreis kreis) {
 		return false;
 	}
 
 	@Override
+	// Vorbedingung: sechseck darf nicht null sein.
+	// Nachbedingung: Gibt true zurueck wenn this in sechseck passt, sonst false.
 	public boolean passtIn(Sechseck sechseck) {
 		double radius = sechseck.getSeitenlaenge();
 		double inkreis = radius * Math.sqrt(3) / 2;
@@ -44,14 +56,18 @@ public class Rechteck extends Grundflaeche {
 	}
 
 	@Override
+	// Vorbedingung: kartondicke darf nicht kleiner 0 sein.
+	// Nachbedingung: Gibt die Flaeche des Rechtecks zurueck und berechnet die Kartondicke ein.
 	public double flaeche(double kartondicke) {
 		return (laenge + 2 * kartondicke) * (breite + 2 * kartondicke);
 	}
 
+	// Nachbedingung: Gibt den Laenge des Rechtecks zurueck.
 	public double getLaenge() {
 		return laenge;
 	}
 
+	// Nachbedingung: Gibt den Breite des Rechtecks zurueck.
 	public double getBreite() {
 		return breite;
 	}
