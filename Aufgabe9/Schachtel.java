61781a4bab3bfe89d848108ef3691c15242ba645
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-16 17:42:27 +0100

    Schachtel.

--- Aufgabe9/Schachtel.java
+++ Aufgabe9/Schachtel.java
@@ -1,4 +1,16 @@
 
-public class Schachtel {
-
+public class Schachtel extends Geschenk{
+	
+	private static final double KARTONDICKE = 0.5;
+	
+	public Schachtel(double hoehe, Grundflaeche grundflaeche) {
+		super("", hoehe, grundflaeche);
+	}
+	
+	public double volumen() {
+		double flaecheAussen = super.getGrundform().flaeche(KARTONDICKE);
+		double hoeheAussen = super.getHoehe() + 2 * KARTONDICKE;
+		return flaecheAussen * hoeheAussen;
+	}
+	
 }
