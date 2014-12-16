0d4f0456924847482e4a6c8f9019aad8e4314d36
djaffry <dave.jaffry@gmail.com>
2014-12-16 21:43:45 +0100

    renamed glas to vase

--- Aufgabe9/Glas.java
+++
@@ -1,8 +0,0 @@
-
-public class Glas extends Geschenk{
-
-	public Glas(String name, double hoehe, double radius) {
-		super(name, hoehe, new Kreis(radius));
-		// TODO Auto-generated constructor stub
-	}
-}
