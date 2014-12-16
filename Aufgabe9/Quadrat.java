1e930ce6fd09992d24bbae439f64d8609f129968
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-16 17:50:44 +0100

    Added Getter

--- Aufgabe9/Quadrat.java
+++ Aufgabe9/Quadrat.java
@@ -11,4 +11,25 @@ public class Quadrat extends Grundflaeche {
 	public double flaeche(double kartondicke) {
 		return Math.pow(seitenlaenge + 2 * kartondicke, 2);
 	}
+	
+	//Quadrat kann haben: Kreis, Quadrat, 
+	public boolean kannBeinhalten(Grundflaeche g) {
+		if(g instanceof Sechseck || g instanceof Rechteck) {
+			return false;
+		}
+		
+		if(g instanceof Kreis) {
+			Kreis k = (Kreis) g;
+			
+		}
+		if(g instanceof Quadrat && this.getSeitenlaenge() > ((Quadrat) g).getSeitenlaenge()) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	public double getSeitenlaenge() {
+		return seitenlaenge;
+	}
 }
