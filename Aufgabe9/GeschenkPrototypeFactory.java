1cb7403913776800d9ff851a9efa60bda1edaa22
Tom Muhm <tom.muhm@gmail.com>
2014-12-16 21:16:36 +0100

    stuff

--- Aufgabe9/GeschenkPrototypeFactory.java
+++ Aufgabe9/GeschenkPrototypeFactory.java
@@ -7,24 +7,21 @@ public class GeschenkPrototypeFactory {
 	public GeschenkPrototypeFactory() {
 		geschenkMap = new HashMap<String, Geschenk>();
 
-
 		geschenkMap.put("kerze0", new Kerze("Kerze 15x10", 15, 10));
 		geschenkMap.put("kerze1", new Kerze("Kerze 20x05", 20, 05));
 		geschenkMap.put("buch0", new Buch("Buch 2x10x4", 2, 10, 4));
 		geschenkMap.put("buch1", new Buch("Buch 3x12x6", 3, 12, 6));
 		geschenkMap.put("wuerfel0", new Wuerfel("Wuerfel 5x5x5", 5, 5));
 		geschenkMap.put("wuerfel1", new Wuerfel("Wuerfel 7x7x7", 7, 7));
-		geschenkMap.put("wein0", new Wuerfel("Wuerfel 7x7x7", 7, 7));
-		geschenkMap.put("wein1", new Wuerfel("Wuerfel 7x7x7", 7, 7));
+		geschenkMap.put("wein0", new Weinflasche("Weinfalsche 20x3", 15, 3));
+		geschenkMap.put("wein1", new Weinflasche("Weinflasche 20x4", 20, 4));
 
 	}
 
 	public Geschenk getGeschenk(String geschenk) {
 		if (geschenkMap.containsKey(geschenk))
-			return geschenkMap.get(geschenk).clone();
-
+			return (Geschenk) geschenkMap.get(geschenk).clone();
 		return null;
 	}
-
 
 }
--- Aufgabe9/Weinflasche.java
+++ Aufgabe9/Weinflasche.java
@@ -4,4 +4,5 @@ public class Weinflasche extends Geschenk {
 	public Weinflasche(String name, double hoehe, double radius) {
 		super(name, hoehe, new Kreis(radius));
 	}
+
 }
