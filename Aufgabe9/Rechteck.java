485fcc448c0e02f71e0ddf81796878a2c9758884
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-16 18:37:03 +0100

    added kannBeinhalten

--- Aufgabe9/Rechteck.java
+++ Aufgabe9/Rechteck.java
@@ -3,6 +3,8 @@ public class Rechteck extends Grundflaeche {
 
 	private double laenge;
 	private double breite;
+	
+	//invariante für laenge > breite setzen!
 
 	public Rechteck(double laenge, double breite) {
 		this.laenge = laenge;
@@ -10,7 +12,29 @@ public class Rechteck extends Grundflaeche {
 	}
 
 	@Override
-	public boolean kannBeinhalten(Grundflaeche grundflaeche) {
+	public boolean kannBeinhalten(Grundflaeche g) {
+		if(g instanceof Kreis || g instanceof Quadrat) {
+			return false;
+		}
+		
+		if(g instanceof Sechseck && this.getLaenge() > 2*((Sechseck) g).getSeitenlaenge() && this.getBreite() > 2*((Sechseck) g).getSeitenlaenge()) {
+			double radius = ((Sechseck) g).getSeitenlaenge();
+			double inkreis = radius * Math.sqrt(3) / 2;
+
+			if(this.getLaenge() >= 2*radius && this.getBreite() >= 2*inkreis) {
+				return true;
+			}
+		}
+		
+		if(g instanceof Rechteck) {
+			double gLaenge = ((Rechteck) g).getLaenge();
+			double gBreite = ((Rechteck) g).getBreite();
+			
+			if(this.getLaenge() >= gLaenge && this.getBreite() >= gBreite) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
