c414d51151cfec4e5538704593764121ba38b18e
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-16 19:09:57 +0100

    meh

--- Aufgabe9/Grundflaeche.java
+++ Aufgabe9/Grundflaeche.java
@@ -5,7 +5,13 @@ public abstract class Grundflaeche {
 		return flaeche(0);
 	}
 
-	public abstract boolean kannBeinhalten(Grundflaeche grundflaeche);
+	public abstract boolean kannBeinhalten(Quadrat quadrat);
+	
+	public abstract boolean kannBeinhalten(Rechteck rechteck);
+	
+	public abstract boolean kannBeinhalten(Kreis kreis);
+	
+	public abstract boolean kannBeinhalten(Sechseck sechseck);
 
 	public abstract double flaeche(double kartondicke);
 
