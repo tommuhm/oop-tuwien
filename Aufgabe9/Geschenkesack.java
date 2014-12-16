353c3f84973a6a0a418689ecaa999d9bae77220b
djaffry <dave.jaffry@gmail.com>
2014-12-16 17:50:07 +0100

    added geschenkesack schachtellager and lots of love

--- Aufgabe9/Geschenkesack.java
+++ Aufgabe9/Geschenkesack.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
 
 public class Geschenkesack {
+	private static Geschenkesack instance = null;
+	private ArrayList<Schachtel> sacklist;
 
+	public Geschenkesack(){
+		sacklist = new ArrayList<Schachtel>();
+	}
+
+	public Geschenkesack(ArrayList geschenklist){
+		sacklist = geschenklist;
+	}
+	
+	public static Geschenkesack getInstance(){
+		if(instance == null) {
+			instance = new Geschenkesack();
+		}
+		return instance;
+	}
+
+	public void addGeschenk(Schachtel schachtel){
+		sacklist.add(schachtel);
+	}
+	
+	public double volumen(){
+		double volumen;
+		for (Schachtel schachtel : sacklist) {   
+		    volumen += schachtel.volumen();
+		}
+		return volumen;
+	}
+
+	public String inhalt(){
+		String inhalt = "\n ########## \n";
+		for (Schachtel schachtel : sacklist) {   
+		    schachtel.getName();
+		}
+	}
 }
--- Aufgabe9/Schachtellager.java
+++ Aufgabe9/Schachtellager.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
 
 public class Schachtellager {
+	private static Schachtellager instance = null;
+	public ArrayList<Schachtel> lagerlist;
 
+	public Schachtellager(){
+		lagerlist = new ArrayList<Schachtel>();
+	}
+
+	public static Schachtellager getInstance(){
+		if(instance == null) {
+			instance = new Schachtellager();
+		}
+		return instance;
+	}
+
+	public void addSchachtel(Schachtel schachtel){
+		lagerlist.add(schachtel);
+	}
+
+	public void verpacke(Geschenk geschenk){
+		boolean stickybit = false;
+		for (Schachtel schachtel : lagerlist) {   
+			if (schachtel.addGeschenk(geschenk)) {
+				schachtel.addGeschenk(geschenk);
+				stickybit = true;
+			}
+		}
+		if (!(stickybit)) //finde schachtel und erstelle und hau geschenk rein
+	}
+
+	public String inventar(){
+		return null;	
+	}
 }
