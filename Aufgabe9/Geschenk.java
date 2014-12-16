6fe7742ce5cd991d3927d04453c72ccc2e5afb41
Tom Muhm <tom.muhm@gmail.com>
2014-12-16 16:44:21 +0100

    implemented geschenk

--- Aufgabe9/Geschenk.java
+++ Aufgabe9/Geschenk.java
@@ -1,4 +1,14 @@
 
-public class Geschenk {
+public abstract class Geschenk {
+
+	private String name;
+	private double hoehe;
+	private Grundform grundform;
+
+	public Geschenk(String name, double hoehe, Grundform grundform) {
+		this.name = name;
+		this.hoehe = hoehe;
+		this.grundform = grundform;
+	}
 
 }
