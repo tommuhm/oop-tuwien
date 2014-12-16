3b9dbc32426b852d6e579059f42a57377bbed467
Tom Muhm <tom.muhm@gmail.com>
2014-12-16 20:53:41 +0100

    brrr

---
+++ Aufgabe9/GeschenkPrototypeFactory.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class GeschenkPrototypeFactory {
+
+	HashMap<String, Geschenk> geschenkMap;
+
+	public GeschenkPrototypeFactory() {
+		geschenkMap = new HashMap<String, Geschenk>();
+
+
+		geschenkMap.put("kerze1", new Kerze("Kerze 15x10", 15, 10));
+		geschenkMap.put("kerze2", new Kerze("Kerze 20x05", 20, 05));
+		geschenkMap.put("buch1", new Buch("Buch 2x10x4", 2, 10, 4));
+		geschenkMap.put("buch2", new Buch("Buch 3x12x6", 3, 12, 6));
+		geschenkMap.put("wuerfel1", new Wuerfel("Wuerfel 5x5x5", 5, 5));
+		geschenkMap.put("wuerfel2", new Wuerfel("Wuerfel 7x7x7", 7, 7));
+		geschenkMap.put("wein1", new Wuerfel("Wuerfel 7x7x7", 7, 7));
+		geschenkMap.put("wein2", new Wuerfel("Wuerfel 7x7x7", 7, 7));
+
+	}
+
+	public Geschenk getGeschenk(String geschenk) {
+		if (geschenkMap.containsKey(geschenk))
+			return geschenkMap.get(geschenk).clone();
+
+		return null;
+	}
+
+
+}
