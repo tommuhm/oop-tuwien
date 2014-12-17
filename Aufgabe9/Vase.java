1ab918dd3cac973f2b0fda6f5cce595a482dc5f8
Ulrich Aschl <ulrich.aschl@gmail.com>
2014-12-17 02:57:38 +0100

    Rechtschreibung.

--- Aufgabe9/Vase.java
+++ Aufgabe9/Vase.java
@@ -1,6 +1,6 @@
 public class Vase extends Geschenk {
 
-	// VOrbedingung: name soll nicht null sein
+	// Vorbedingung: name soll nicht null sein
 	// Invariante: hoehe muss groesser als 0 sein
 	// Invariante: radius muss groesser als 0 sein
 	public Vase(String name, double hoehe, double radius) {
